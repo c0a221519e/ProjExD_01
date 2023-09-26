f919b3f7ebada0fa2dc01129c40a49e8aef2597e
Ricky <c0a221519e@edu.teu.ac.jp>
2023-09-26 17:32:54 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -12,21 +12,34 @@ def main():
 
     kouka_img_3 = pg.transform.flip(kouka_img_3,True,False)
     kouka_img_3_1 = pg.transform.rotozoom(kouka_img_3,10,1.0)
-    x=0
-    y=0
+
     tmr = 0
+    x = 0
+    koukaton_tmr = 0
     koukaton = [kouka_img_3,kouka_img_3_1]
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         clock.tick()
 
+        if x == 1599:
+            x = 0
 
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img_reverse, [1600-x, 0])
+
+        if koukaton_tmr <= 50:
+            screen.blit(koukaton[0],[300,200])
+        elif 50 < koukaton_tmr <= 100: 
+            screen.blit(koukaton[1],[300,200])
+        else:
+            koukaton_tmr = 0
 
 
         pg.display.update()
         tmr += 1     
-        x += 1  
+        x += 1
+        koukaton_tmr += 1 
         clock.tick(1000)
 
 
